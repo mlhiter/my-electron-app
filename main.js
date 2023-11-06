0bf786bb009922f60f7ee8e84198c0ff35a350aa
Alex Lee <3076438032@qq.com>
2023-11-06 22:47:49 +0800

    feat: chore other

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
 const path = require('path')
 
+// 新建窗口实例
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -11,10 +12,11 @@ const createWindow = () => {
   })
   win.loadFile('index.html')
 }
-//ready后挂载窗口
+// app模块ready后挂载窗口
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
-  //没有窗口激活应用的话就打开窗口
+  // 没有窗口激活应用的话就打开窗口
   app.on('activate', () => {
     //创建窗口只能在ready后，所以放在里面
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
--- preload.js
+++ preload.js
@@ -1,10 +1,20 @@
-window.addEventListener('DOMContentLoaded', () => {
-  //辅助函数，辅助输出替换文本
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency])
-  }
+// window.addEventListener('DOMContentLoaded', () => {
+//   //辅助函数，辅助输出替换文本
+//   const replaceText = (selector, text) => {
+//     const element = document.getElementById(selector)
+//     if (element) element.innerText = text
+//   }
+//   for (const dependency of ['chrome', 'node', 'electron']) {
+//     replaceText(`${dependency}-version`, process.versions[dependency])
+//   }
+// })
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping'), //包裹一下防止出现安全问题（渲染进程可以无限制向主进程发送信息，如果直接暴露的话）
+
+  // 除函数之外，我们也可以暴露变量
 })
